335cc09b486aa1809bf06c1eb84d23026117e68a
VeryEpicEnginar <florantia@proton.me>
2026-01-09 23:34:16 +0300

    Refactor ELF header structures for 32-bit support

--- kernel/exe/elf.h
+++ kernel/exe/elf.h
@@ -1,24 +1,23 @@
 #pragma once
 #include <stdint.h>
-#include <stddef.h>
 
 #define ELF_MAGIC 0x464C457F
-#define PT_LOAD   1
+
+#define ELF_PT_LOAD 1
 
 typedef struct {
     uint32_t magic;
-    uint8_t  elf_class;
-    uint8_t  data;
+    uint8_t  bits;
+    uint8_t  endian;
     uint8_t  version;
-    uint8_t  osabi;
-    uint8_t  abi_version;
-    uint8_t  pad[7];
+    uint8_t  abi;
+    uint8_t  pad[8];
     uint16_t type;
     uint16_t machine;
     uint32_t version2;
-    uint64_t entry;
-    uint64_t phoff;
-    uint64_t shoff;
+    uint32_t entry;
+    uint32_t phoff;
+    uint32_t shoff;
     uint32_t flags;
     uint16_t ehsize;
     uint16_t phentsize;
@@ -26,17 +25,15 @@ typedef struct {
     uint16_t shentsize;
     uint16_t shnum;
     uint16_t shstrndx;
-} __attribute__((packed)) elf64_hdr_t;
+} Elf32_Ehdr;
 
 typedef struct {
     uint32_t type;
+    uint32_t offset;
+    uint32_t vaddr;
+    uint32_t paddr;
+    uint32_t filesz;
+    uint32_t memsz;
     uint32_t flags;
-    uint64_t offset;
-    uint64_t vaddr;
-    uint64_t paddr;
-    uint64_t filesz;
-    uint64_t memsz;
-    uint64_t align;
-} __attribute__((packed)) elf64_phdr_t;
-
-uint64_t elf_load(void *image);
+    uint32_t align;
+} Elf32_Phdr;
