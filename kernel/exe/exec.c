182616741238ae8422eeb4c07c265db0896329d6
VeryEpicEnginar <florantia@proton.me>
2026-01-10 15:18:36 +0300

    Refactor exec function and stack allocation logic

--- kernel/exe/exec.c
+++ kernel/exe/exec.c
@@ -2,40 +2,23 @@
 #include "elf.h"
 #include "../mm/pmm.h"
 #include "../mm/vmm32.h"
-#include "../core/panic.h"
-#include "../lib/string.h"
-#include <stdint.h>
 
-#define USER_STACK_TOP   0xBFFFE000
-#define USER_STACK_SIZE  0x4000      /* 16 KB */
+#define USER_STACK_TOP  0xBFFFE000
+#define USER_STACK_SIZE 0x4000
 
-extern void exec_enter_user(uint32_t entry, uint32_t user_stack);
-
-void exec_run(void* elf_image)
+void exec(const void *image)
 {
-    uint32_t entry = (uint32_t)elf_load(elf_image);
+    uint32_t entry = elf_load(image);
 
-    uint32_t stack_bottom = USER_STACK_TOP - USER_STACK_SIZE;
-
-    for (uint32_t va = stack_bottom + 0x1000;
-         va < USER_STACK_TOP;
-         va += 0x1000)
-    {
-        uint32_t pa = (uint32_t)pmm_alloc_pages(1);
-
-        if (!pa)
-            panic("exec: out of memory (stack)");
+    for (uint32_t off = 0; off < USER_STACK_SIZE; off += 0x1000) {
+        uint32_t phys = pmm_alloc_pages(1);
 
         vmm32_map_page(
-            va,
-            pa,
+            USER_STACK_TOP - off,
+            phys,
             VMM_PRESENT | VMM_USER | VMM_WRITABLE
         );
-
-        memset((void*)va, 0, 0x1000);
     }
 
     exec_enter_user(entry, USER_STACK_TOP);
-
-    panic("exec: returned from user mode");
 }
